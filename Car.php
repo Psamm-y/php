2fbf1eb898613e15e2e61099e471233f36023040
Psammy <ampadupsamuel1@gmail.com>
2025-02-07 12:25:32 +0000

    Update files

--- Car.php
+++ Car.php
@@ -7,5 +7,11 @@ class Car extends Vehicle{
     parent::__construct($vehicleId, $model, $fuelLevel);
     $this->fuelEfficiency = $fuelEfficincy;
   }
+
+  public function calculateRange(){
+    $range=$this->fuelLevel * $this->fuelEfficiency;
+    echo "The range of the car ($this->model) is $range km\n";
+    return range;
+  }
 }
 ?>
