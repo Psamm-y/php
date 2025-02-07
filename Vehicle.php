75f5abc56652e396cc7b6c5c4dbb89d8175560ba
Psammy <ampadupsamuel1@gmail.com>
2025-02-07 12:18:49 +0000

    Add Vehicle.php and Car.php files

---
+++ Vehicle.php
@@ -0,0 +1,18 @@
+<?php
+  class Vehicle{
+    protected $vehicleId;
+    protected $model;
+    protected $fuelLevel;
+
+    public function __construct($vehicleId, $model, $fuelLevel){
+      $this->vehicleId = $vehicleId;
+      $this->model = $model;
+      $this->fuelLevel = $fuelLevel;
+    }
+
+    public function refuel($litres){
+      $this->fuelLevel += $litres;
+      echo "Refueled $litres litres. New fuel Level: $this->fuelLevel\n";
+    }
+  }
+?>
