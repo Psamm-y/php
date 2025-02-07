0fb48faa10f064e45d0dc1624569dd9a2ae9d608
Psammy <ampadupsamuel1@gmail.com>
2025-02-07 13:29:56 +0000

    Update

--- FullTimeEmployee.php
+++ FullTimeEmployee.php
@@ -3,10 +3,17 @@ require_once 'Employee.php';
 class FullTimeEmployee extends Employee{
 private $salary;
 
+public function getSalary(){
+  return $this->salary;
+}
+
 public function __construct($name, $employeeId, $salary){
   $this->name=$name;
   $this->employeeId=$employeeId;
   $this->salary=$salary;
 }
+
+public function calculatePay(){
+ echo "Full Time Employee: ". getSalary;
 }
 ?>
